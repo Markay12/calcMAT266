2e298bf7df345f78bcc467d786afbb262e7389df
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-11-13 16:31:52 -0700

    Main constructor file

---
+++ GradeCalculator.java
@@ -0,0 +1,43 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class GradeCalculator extends JPanel implements KeyListener, ActionListener{
+
+  //instance variables
+  private double exam1;
+  private double exam2;
+  private double exam3;
+  private double quiz;
+  private double homew;
+  private double skill;
+  private double finalg;
+  private double finalMathGrade;
+
+
+  //main constructor
+  public GradeCalculator(){
+
+    JPanel exam1box = new JPanel();
+    exam1box.setSize(10, 10);
+
+  }
+
+
+  public void keyTyped(KeyEvent e){
+
+  }
+  public void keyReleased(KeyEvent e){
+
+  }
+  public void actionPerformed(ActionEvent e){
+
+  }
+  public void keyPressed(KeyEvent e){
+
+  }
+
+
+
+
+}
